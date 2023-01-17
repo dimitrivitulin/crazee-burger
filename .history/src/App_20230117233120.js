29807c3df2625d6c963709004b2570199ca8edac
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-17 23:54:44 +0100

    App.js > Add my firstname

--- .history/src/App_20230117233120.js
+++ .history/src/App_20230117233120.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Salut Dim</p>
+        <p>
+          Salut Dim
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
