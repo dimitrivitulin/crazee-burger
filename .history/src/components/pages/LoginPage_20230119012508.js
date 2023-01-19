d417e226fa8dea977e89389caca1b320751c48d4
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-19 01:35:07 +0100

    ✨F01: LOGIN> Add elements of form

--- .history/src/components/pages/LoginPage_20230119012508.js
+++ .history/src/components/pages/LoginPage_20230119012508.js
@@ -1,35 +1,35 @@
 import { useState } from "react";
 
 export default function LoginPage() {
+  
   // State
   const [prenom, setPrenom] = useState("");
 
-  // Comportement
+// Comportement
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (prenom) {
+  
+    if(prenom){
       alert(`Bonjour ${prenom}`);
     }
-    setPrenom("");
+    setPrenom("")
   };
 
   // Affichage
   return (
-    <div className="App">
+     <div className="App">
       <h1>Bienvenue chez nous !</h1>
       <br />
-      <h2>Connectez-vous</h2>
+      <h3>Connectez-vous</h3>
       <form onSubmit={handleSubmit}>
-        <input
-          value={prenom}
+        <input 
+          value={prenom} 
           onChange={(e) => setPrenom(e.target.value)}
-          type="text"
-          placeholder="Entrez votre prénom..."
+          type="text" placeholder="Entrez votre prénom..." 
           required
         />
         <button>Accédez à votre espace</button>
       </form>
     </div>
-  );
+  )
 }
