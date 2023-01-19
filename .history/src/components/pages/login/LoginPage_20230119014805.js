e394ccbaa5321fe08ffedf0435e99a4f392d9b98
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-19 21:02:05 +0100

    🔥 F01: Remove .history

--- .history/src/components/pages/login/LoginPage_20230119014805.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import LoginForm from "./LoginForm";
-
-export default function LoginPage() {
-
-  // Affichage
-  return <LoginForm />
-}
--- .history/src/components/pages/login/LoginPage_20230119014823.js
+++
@@ -1,8 +0,0 @@
-
-import LoginForm from "./LoginForm";
-
-export default function LoginPage() {
-
-  // Affichage
-  return <LoginForm />
-}
