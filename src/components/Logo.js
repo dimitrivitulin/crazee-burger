026429393532a597d6ca9ddf2a2b7596745d8245
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-22 18:38:55 +0100

    💄 F03: LOGIN >Styling login logo with Style components

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,40 @@
+import styled from "styled-components"
+import { theme } from "../theme"
+import logo from "./../assets/images/F03-logo-orange.png"
+
 export default function Logo() {
   return (
-    <div>
-      <p>logo</p>
-      <img src="" alt="" />
-    </div>
+    <LogoWrapper>
+      <LogoTitle>
+        <LogoText>CRAZEE</LogoText>
+        <ImgLogoWrapper>
+          <ImgLogo src={logo} alt="" />
+        </ImgLogoWrapper>
+        <LogoText>BURGER</LogoText>
+      </LogoTitle>
+    </LogoWrapper>
   )
 }
+
+const LogoWrapper = styled.div`
+  font-family: "Amatic SC", cursive;
+  font-weight: 700;
+`
+const LogoTitle = styled.div`
+  color: ${theme.colors.primary_burger};
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.75rem;
+`
+const LogoText = styled.h2`
+  line-height: 0;
+  font-size: 9vh;
+`
+const ImgLogoWrapper = styled.span`
+  height: 100%;
+`
+const ImgLogo = styled.img`
+  height: 100%;
+`
