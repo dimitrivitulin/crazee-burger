e33b97c605efa741bd1485a1f4302f8f4532011a
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-22 18:00:52 +0100

    💄 F03: LOGIN> Styling LoginPageWrapper with Style Components

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,12 +1,29 @@
+import styled from "styled-components"
 import Logo from "../../Logo"
 import LoginForm from "./LoginForm"
+import burgerBackground from "./../../../assets/images/F03-burger-background.jpg"
 
 export default function LoginPage() {
   // Affichage
   return (
-    <div>
+    <LoginPageWrapper>
       <Logo />
       <LoginForm />
-    </div>
+    </LoginPageWrapper>
   )
 }
+
+const LoginPageWrapper = styled.div`
+  height: 100vh;
+  border: 1px solid orange;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  background-image: url(${burgerBackground});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`
