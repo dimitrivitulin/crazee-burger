38d7814e4b68376152aaa62cf3b586763e76e3db
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-01-27 15:02:23 +0100

    ✨ F04: OrderPage > define OrderPage base styling and Main Layout styling

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,14 +1,22 @@
-import { Link, useParams } from "react-router-dom"
+import styled from "styled-components"
+import Main from "../../reusable-ui/Main/Main"
+import Navbar from "../../reusable-ui/Navbar/Navbar"
 
 export default function OrderPage() {
-  const { username } = useParams()
-
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <Navbar />
+      <Main />
+    </OrderPageStyled>
   )
 }
+
+const OrderPageStyled = styled.div`
+  background: orange;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 25px 0;
+`
---
+++ src/components/reusable-ui/Main/Main.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export default function Main() {
+  return <MainStyled>Main</MainStyled>
+}
+
+const MainStyled = styled.div`
+  background: #f5f5f7;
+  height: 100%;
+  width: 100%;
+  max-width: 1400px;
+  box-shadow: inset 0px 8px 20px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 0px 0px 15px 15px;
+`
