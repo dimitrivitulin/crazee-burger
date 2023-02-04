0a6923621b40468885c15f90cc4fbdff6e0e72fa
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-04 01:35:05 +0100

    💄 F05: OrderPage >Main >Menu styling with Grid

--- src/components/pages/order/Main/Menu/Menu.js
+++ src/components/pages/order/Main/Menu/Menu.js
@@ -1,12 +1,24 @@
+import styled from "styled-components"
 import { fakeMenu2 } from "../../../../../fakeData/fakeMenu"
 import Card from "./Card"
 export default function Menu() {
   const burgers = fakeMenu2
   return (
-    <div>
+    <MenuStyled>
       {burgers.map((burger) => (
         <Card burger={burger} />
       ))}
-    </div>
+    </MenuStyled>
   )
 }
+
+const MenuStyled = styled.div`
+  width: 100%;
+  background-color: red;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 200px);
+  grid-auto-rows: 330px;
+  gap: 85px;
+  padding: 50px;
+  justify-content: center;
+`
