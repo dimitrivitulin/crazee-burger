afcf10d74e4e9b0d94d54c5b520c5a3a4303e0fe
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-04 15:08:35 +0100

    💄 F05: OrderPage > Main > styling Card

--- src/components/pages/order/Main/Menu/Card.js
+++ src/components/pages/order/Main/Menu/Card.js
@@ -1,21 +1,44 @@
 import styled from "styled-components"
+import { theme } from "../../../../../theme"
 
+import PrimaryButton from "./../../../../reusable-ui/PrimaryButton"
 export default function Card({ burger }) {
   return (
     <CardStyled key={burger.id}>
       <img src={burger.imageSource} alt={burger.title} />
       <h2>{burger.title}</h2>
-      <h3>{burger.price}€</h3>
+      <div className="bottom-card">
+        <h3>{burger.price}€</h3>
+        <PrimaryButton label={"Ajouter"} />
+      </div>
     </CardStyled>
   )
 }
 
 const CardStyled = styled.div`
-  background-color: blue;
-  padding-top: 50px;
+  background-color: ${theme.colors.white};
+  padding: 50px 20px 10px 20px;
+  border-radius: ${theme.borderRadius.extraRound};
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+  overflow: hidden;
+  box-sizing: content-box;
   img {
     width: 200px;
     height: 145px;
     object-fit: contain;
+    padding-top: 15px;
+  }
+  h2 {
+    font-family: "Amatic SC";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 45px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  .bottom-card {
+    display: flex;
   }
 `
