4cb3813393af002844c7f3dc1ec76a09dc783919
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-04 18:07:23 +0100

    💄 F05: OrderPage > Main > styling Card component

--- src/components/pages/order/Main/Menu/Card.js
+++ src/components/pages/order/Main/Menu/Card.js
@@ -1,15 +1,21 @@
 import styled from "styled-components"
 import { theme } from "../../../../../theme"
+import {
+  formatPrice,
+  replaceFrenchCommaWithDot,
+} from "../../../../../utils/maths"
 
 import PrimaryButton from "./../../../../reusable-ui/PrimaryButton"
 export default function Card({ burger }) {
   return (
     <CardStyled key={burger.id}>
       <img src={burger.imageSource} alt={burger.title} />
-      <h2>{burger.title}</h2>
-      <div className="bottom-card">
-        <h3>{burger.price}€</h3>
-        <PrimaryButton label={"Ajouter"} className={"card-btn"} />
+      <div className="info-card">
+        <h2>{burger.title}</h2>
+        <div className="bottom-info-card">
+          <h3>{formatPrice(replaceFrenchCommaWithDot(burger.price))}</h3>
+          <PrimaryButton label={"Ajouter"} className={"card-btn"} />
+        </div>
       </div>
     </CardStyled>
   )
@@ -20,31 +26,49 @@ const CardStyled = styled.div`
   padding: 50px 20px 10px 20px;
   border-radius: ${theme.borderRadius.extraRound};
   box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
-  overflow: hidden;
-  box-sizing: content-box;
   img {
     width: 200px;
     height: 145px;
     object-fit: contain;
     padding-top: 15px;
   }
-  h2 {
-    font-family: "Amatic SC";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 36px;
-    line-height: 45px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  .bottom-card {
+
+  .info-card {
+    width: 200px;
+    height: 110px;
     display: flex;
-    .card-btn {
-      width: 95px;
-      padding: 12px 0;
-      span {
-        font-size: 11px;
+    flex-direction: column;
+    padding: 5px;
+    h2 {
+      font-family: "Amatic SC";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 36px;
+      line-height: 45px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+    .bottom-info-card {
+      flex-grow: 1;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      h3 {
+        font-family: "Open Sans";
+        font-weight: ${theme.fonts.weights.regular};
+        font-size: 16px;
+        line-height: 22px;
+        color: ${theme.colors.primary};
+      }
+      .card-btn {
+        width: 50%;
+        padding: 12px 0;
+        span {
+          font-family: Arial, Helvetica, sans-serif;
+          font-weight: 700;
+          font-size: 11px;
+        }
       }
     }
   }
