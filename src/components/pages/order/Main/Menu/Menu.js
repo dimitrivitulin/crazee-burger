4ecf810eab476287c58fe2e9efefc29828631600
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-05 17:21:21 +0100

    💄 F05: OrderPage > Menu >styling scrollbar

--- src/components/pages/order/Main/Menu/Menu.js
+++ src/components/pages/order/Main/Menu/Menu.js
@@ -26,4 +26,26 @@ const MenuStyled = styled.div`
   padding: 50px 50px 150px;
   justify-content: center;
   overflow-y: scroll;
+  &::-webkit-scrollbar {
+    visibility: hidden;
+    width: 8px;
+  }
+  &:hover {
+    &::-webkit-scrollbar-track {
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+      border-radius: 10px;
+      background-color: ${theme.colors.background_white};
+    }
+
+    &::-webkit-scrollbar {
+      visibility: visible;
+      background-color: ${theme.colors.background_white};
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+      background-color: ${theme.colors.greySemiDark};
+    }
+  }
 `
