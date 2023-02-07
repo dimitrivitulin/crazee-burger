796ae0dc44641c514aa2dfd6ca4972e7d962c5a0
dimitrivitulin <79697501+dimitrivitulin@users.noreply.github.com>
2023-02-08 00:42:59 +0100

    ✨ F06: ORDER > RightSide >Add ToogleButton and ToastContainer

--- src/components/pages/order/Navbar/RightSide/RightSide.js
+++ src/components/pages/order/Navbar/RightSide/RightSide.js
@@ -1,9 +1,15 @@
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
 import styled from "styled-components"
 import Profile from "./Profile"
+import { theme } from "../../../../../theme"
+import ToggleButton from "./ToggleButton"
 
 export default function RightSide({ username }) {
   return (
     <RightSideStyled>
+      <ToggleButton />
+      <ToastContainer className="toaster" bodyClassName="body-toast" />
       <Profile username={username} />
     </RightSideStyled>
   )
@@ -11,4 +17,20 @@ export default function RightSide({ username }) {
 
 const RightSideStyled = styled.div`
   display: flex;
+  .toaster {
+    max-width: 300px;
+  }
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `
